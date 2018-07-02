2750aa0122930292ffcbd388eb8da8b3c6ff646a
Gustavo Rayos <gustavorayos@outlook.com>
2018-07-02 17:39:35 -0500

    troubleshooting

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+function detectMobile() {
+    var x = document.getElementById('mobileCheck');
+    var y = document.getElementById('desktopCheck');
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobi/i.test(navigator.userAgent)) {
+        //alert("You are on mobile");
+        x.className += ' w3-show';
+    } else {
+        //alert("You are on desktop");
+        y.className += ' w3-show';
+    }
+}
